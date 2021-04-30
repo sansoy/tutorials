c697b546f9575ab0c42e5673248cc8abafecec9d
Sabri Sansoy <ssansoy@gmail.com>
2021-04-30 10:44:23 -0600

    Update convert_checkpoint_to_edgetpu_tflite.sh

--- docker/object_detection/scripts/convert_checkpoint_to_edgetpu_tflite.sh
+++ docker/object_detection/scripts/convert_checkpoint_to_edgetpu_tflite.sh
@@ -48,8 +48,7 @@ source "$PWD/constants.sh"
 mkdir -p "${OUTPUT_DIR}"
 
 echo "GENERATING label file..."
-echo "0 Abyssinian" >> "${OUTPUT_DIR}/labels.txt"
-echo "1 american_bulldog" >> "${OUTPUT_DIR}/labels.txt"
+echo "0 polaris" >> "${OUTPUT_DIR}/labels.txt"
 
 echo "EXPORTING frozen graph from checkpoint..."
 python object_detection/export_tflite_ssd_graph.py \
